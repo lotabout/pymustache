27ecb84285a2f9aa3344dd08894b73f8c764edaa
zhang_ji <zhang_ji@worksap.co.jp>
2016-01-21 17:09:17 +0800

    basic parser and renderer done

--- src/mustache.py
+++ src/mustache.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import re
+
+DEFAULT_DELIMITERS = ('{{', '}}')
+EMPTYSTRING = ""
 
 #==============================================================================
 # Context lookup.
@@ -28,6 +32,36 @@ def lookup(var_name, contexts=()):
             continue
     return None
 
+def html_escape(text):
+    return text
+
+#==============================================================================
+# Compilation
+# To compile a template into a tree of tokens, using the given delimiters.
+re_delimiters = {}
+
+def delimiters_to_re(delimiters):
+    """convert delimiters to corresponding regular expressions"""
+
+    # caching
+    if delimiters in re_delimiters:
+        re_tag = re_delimiters[delimiters]
+    else:
+        open_tag, close_tag = delimiters
+
+        # escape
+        open_tag = ''.join([c if c.isalnum() else '\\' + c for c in open_tag])
+        close_tag = ''.join([c if c.isalnum() else '\\' + c for c in close_tag])
+
+        re_tag = re.compile(open_tag + r'([#^>&{/!=]?)\s*(.*?)\s*([}=]?)' + close_tag, re.DOTALL)
+        re_delimiters[delimiters] = re_tag
+
+    return re_tag
+
+class SyntaxError(Exception):
+    pass
+        
+
 def compiled(template, delimiters):
     """Compile a template into token tree
 
@@ -36,9 +70,91 @@ def compiled(template, delimiters):
     :returns: the root token
 
     """
-    return None
+    re_tag = delimiters_to_re(delimiters)
 
-def render(template, context, partials, delimiters=None):
+    # variable to save states
+    tokens = []
+    index = 0
+    sections = []
+    tokens_stack = []
+
+    m = re_tag.search(template, index)
+
+    while m is not None:
+        if m.start() > index:
+            tokens.append(Token('str', Token.LITERAL, template[index:m.start()]))
+        # parse token
+        prefix, name, suffix = m.groups()
+        token = None
+
+        if prefix == '=' and postfix == '=':
+            # {{=| |=}} to change delimiters
+            delimiters = re.split(r'\s+', name)
+            if len(delimiters) != 2:
+                raise SyntaxError('Invalid new delimiter definition: ' + m.group())
+            re_tag = delimiters_to_re(delimiters)
+
+        elif prefix == '{' and suffix == '}':
+            # {{{ variable }}}
+            token = Token(name, Token.VARIABLE, name)
+            token.escape = True
+
+        elif prefix == '' and suffix == '':
+            # {{ name }}
+            token = Token(name, Token.VARIABLE, name)
+
+        elif suffix != '' and suffix != None:
+            raise SyntaxError('Invalid token: ' + m.group())
+
+        elif prefix == '&':
+            # {{& escaped variable }}
+            token = Token(name, Token.VARIABLE, name)
+            token.escape = True
+
+        elif prefix == '!':
+            # {{! comment }}
+            token = Token(name, Token.COMMENT)
+
+        elif prefix == '>':
+            # {{> partial}}
+            token = Token(name, Token.PARTIAL, name)
+
+        elif prefix == '#' or prefix == '^':
+            # {{# section }} or # {{^ inverted }}
+            token = Token(name, Token.SECTION if prefix == '#' else Token.INVERTED, name)
+            tokens.append(token)
+
+            # save the tokens onto stack
+            token = None
+            tokens_stack.append(tokens)
+            tokens = []
+
+            sections.append((name, prefix, m.end()))
+
+        elif prefix == '/':
+            tag_name, sec_type, text_end = sections.pop()
+            if tag_name != name:
+                raise SyntaxError("unclosed tag: '" + name + "' Got:" + m.group())
+
+            children = tokens
+            tokens = tokens_stack.pop()
+
+            tokens[-1].text = template[text_end:m.start()]
+            tokens[-1].children = children
+
+        else:
+            raise SyntaxError('Unknown tag: ' + m.group())
+
+        if token is not None:
+            tokens.append(token)
+
+        index = m.end()
+        m = re_tag.search(template, index)
+
+    tokens.append(Token('str', Token.LITERAL, template[index:]))
+    return Token('root', Token.ROOT, children=tokens)
+
+def render(template, contexts, partials, delimiters=None):
     """TODO: Docstring for render.
 
     :template: TODO
@@ -48,9 +164,9 @@ def render(template, context, partials, delimiters=None):
     :returns: A parsed string
 
     """
-    delimiters = DEFAULT_DELIMITERS if delimiters is not None else delimiters
+    delimiters = DEFAULT_DELIMITERS if delimiters is None else delimiters
     parent_token = compiled(template, delimiters)
-    return parent_token.render(context, partials)
+    return parent_token.render(contexts, partials)
 
 #==============================================================================
 # Token
@@ -67,9 +183,9 @@ class Token():
     COMMENT   = 4
     PARTIAL   = 5
     ROOT      = 6
-    EMPTYSTRING = ""
 
-    def __init__(self, type=LITERAL, value=None, text='', children=None):
+    def __init__(self, name, type=LITERAL, value=None, text='', children=None):
+        self.name = name
         self.type = type
         self.value = value
         self.text = text
@@ -157,7 +273,7 @@ class Token():
         try:
             partial = partials[self.value]
         except KeyError as e:
-            raise ValueError('partial not found: ' + self.value)
+            raise SyntaxError('partial not found: ' + self.value)
 
         return render(partial, contexts, partials, self.delimiter)
 
@@ -169,19 +285,91 @@ class Token():
         :returns: A string contains the rendered result
 
         """
-        if self.type == LITERAL:
+        if self.type == self.LITERAL:
             return self._render_literal(contexts, partials)
-        elif self.type == VARIABLE:
+        elif self.type == self.VARIABLE:
             return self._render_variable(contexts, partials)
-        elif self.type == SECTION:
+        elif self.type == self.SECTION:
             return self._render_section(contexts, partials)
-        elif self.type == INVERTED:
+        elif self.type == self.INVERTED:
             return self._render_inverted(contexts, partials)
-        elif self.type == COMMENT:
+        elif self.type == self.COMMENT:
             return self._render_comments(contexts, partials)
-        elif self.type == PARTIAL:
+        elif self.type == self.PARTIAL:
             return self._render_partials(contexts, partials)
-        elif self.type == ROOT:
+        elif self.type == self.ROOT:
             return self._render_children(contexts, partials)
         else:
             raise TypeError('Invalid Token Type')
+
+    def _type_string(self):
+        if self.type == self.LITERAL:
+            return 'L'
+        elif self.type == self.VARIABLE:
+            return 'V'
+        elif self.type == self.COMMENT:
+            return 'C'
+        elif self.type == self.SECTION:
+            return 'S'
+        elif self.type == self.INVERTED:
+            return 'I'
+        elif self.type == self.PARTIAL:
+            return 'P'
+        elif self.type == self.ROOT:
+            return 'R'
+        else:
+            return 'Unknown'
+
+    def _get_str(self, indent):
+        ret = []
+        ret.append(' '*indent + '[(')
+        ret.append(self._type_string())
+        ret.append(',')
+        ret.append(self.name)
+        if self.value:
+            ret.append(',')
+            ret.append(repr(self.value))
+        ret.append(')')
+        if self.children:
+            for c in self.children:
+                ret.append('\n')
+                ret.append(c._get_str(indent+4))
+        ret.append(']')
+        return ''.join(ret)
+
+    def __str__(self):
+        return self._get_str(0)
+
+
+text = """
+<h1>{{header}}</h1>
+{{#bug}}
+{{/bug}}
+
+{{#items}}
+  {{#first}}
+    <li><strong>{{name}}</strong></li>
+  {{/first}}
+  {{#link}}
+    <li><a href="{{url}}">{{name}}</a></li>
+  {{/link}}
+{{/items}}
+
+{{#empty}}
+  <p>The list is empty.</p>
+{{/empty}}
+"""
+
+x = compiled(text, DEFAULT_DELIMITERS)
+#print(x)
+context = [{
+  "header": "Colors",
+  "items": [
+      {"name": "red", "first": True, "url": "#Red"},
+      {"name": "green", "link": True, "url": "#Green"},
+      {"name": "blue", "link": True, "url": "#Blue"}
+  ],
+  "empty": False
+}]
+
+print(render(text, context, []))
